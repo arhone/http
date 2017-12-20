cf92567b21802dd30645a67103c4275daab1f60c
Алексей Арх <info@arh.one>
2017-12-20 21:56:26 +0300

    Http

--- Http.php
+++ Http.php
@@ -9,5 +9,58 @@ namespace arhone\http;
  */
 class Http implements HttpInterface {
 
+    /**
+     * @var Request
+     */
+    protected $Request;
+
+    /**
+     * @var Response
+     */
+    protected $Response;
+
+    /**
+     * Http constructor.
+     * @param Request $Request
+     * @param Response $Response
+     */
+    public function __construct (Request $Request, Response $Response) {
+
+        $this->Request  = clone $Request;
+        $this->Response = clone $Response;
+
+    }
+
+    /**
+     * Возвращает объект
+     *
+     * @param $name
+     * @return mixed
+     */
+    public function __get ($name) {
+
+        if (isset($this->{$name})) {
+
+            return clone $this->{$name};
+
+        }
+
+    }
+
+    /**
+     * Задаёт объект
+     *
+     * @param $name
+     * @param $value
+     */
+    public function __set ($name, $value) {
+
+        if (isset($this->{$name}) && ($value instanceof Request || $value instanceof Response)) {
+
+            $this->{$name} = clone $value;
+
+        }
+
+    }
 
 }
